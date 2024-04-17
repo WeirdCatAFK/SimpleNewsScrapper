cee68496e9eca129c0eb715a0beb30814cbb237c
Daniel <pinedadpt@gmail.com>
2024-04-17 06:09:05 -0600

    Another Big test preparation

--- createDatabase.py
+++ createDatabase.py
@@ -9,7 +9,38 @@ def main():
     print(f'database {database} created successfully')
     
     # Determine our queries
-    search_queries = [ ... ]  # Your list of search queries
+    search_queries = [
+    'Asaltos a camiones de carga en',
+    'Robos a camiones de transporte de mercancías en',
+    'Incidentes de robo a mano armada a camiones en',
+    'Noticias sobre bandas criminales que roban camiones en',
+    'Casos destacados de asaltos a camiones en zonas urbanas en',
+    'Aumento de robos a camiones en',
+    'Respuesta de las autoridades ante el aumento de asaltos a camiones en',
+    'Impacto económico de los robos a camiones en la industria del transporte en',
+    'Carreteras Peligrosas en',
+    'Robos a mano armada en carreteras principales en',
+    'Casos de secuestros exprés en carreteras transitadas en',
+    'Falsos retenes en',
+    'Incidentes de vandalismo en vehículos en',
+    'Asaltos a camiones de carga durante la noche en',
+    'Robos a camiones de transporte de mercancías durante el día en',
+    'Incidentes de robo a mano armada a camiones en autopistas en',
+    'Noticias sobre bandas criminales que roban camiones en áreas rurales en',
+    'Casos destacados de asaltos a camiones en zonas industriales en',
+    'Aumento de robos a camiones en áreas metropolitanas en',
+    'Respuesta de las autoridades ante el aumento de asaltos a camiones en regiones fronterizas en',
+    'Impacto económico de los robos a camiones en la cadena de suministro en',
+    'Carreteras Peligrosas en días festivos en',
+    'Robos a mano armada en carreteras secundarias en días laborables en',
+    'Casos de secuestros exprés en carreteras aisladas en fines de semana en',
+    'Falsos retenes en áreas de construcción en',
+    'Incidentes de vandalismo en vehículos de carga en',
+    'Asaltos a camiones de carga con violencia en',
+    'Robos a camiones de transporte de mercancías con intimidación en',
+    'Incidentes de robo a mano armada a camiones con toma de rehenes en',
+    'Noticias sobre bandas criminales que roban camiones con armas de fuego en',
+    'Casos destacados de asaltos a camiones en áreas de descanso en']  # Your list of search queries
     
     # Initialize an empty list to store all news
     all_the_news = []
@@ -27,6 +58,9 @@ def main():
         
         # Write the current batch of news URLs to the database
         newsSearcher.writeResultsToDatabase(database, 'searchResults', news_urls)
+        
+        #Create the table where we will store our content
+        webScrapper.createDB(database,'content')
         
         # Process news URLs in batches
         for i in range(0, len(news_urls), batch_size):
